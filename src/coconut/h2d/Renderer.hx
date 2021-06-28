9de98496ce2a7d4f8f7741755c68a3cf7a845caa
Juraj Kirchheim <back2dos@gmail.com>
2021-06-28 16:11:06 +0200

    Adjust to coconut.diffing edge.

--- src/coconut/h2d/Renderer.hx
+++ src/coconut/h2d/Renderer.hx
@@ -4,107 +4,94 @@ import coconut.diffing.*;
 import h2d.*;
 
 class Renderer {
-  
-  static var DIFFER = new coconut.diffing.Differ(new H2DBackend());
+
+  static var BACKEND = new H2DBackend();
 
   static public function mountInto(target:Object, virtual:RenderResult)
-    DIFFER.render([virtual], target);
+    new Root(target, BACKEND, virtual);
 
   static public function getNative(view:View):Null<Object>
     return getAllNative(view)[0];
 
   static public function getAllNative(view:View):Array<Object>
-    return switch @:privateAccess view._coco_lastRender {
-      case null: [];
-      case r: r.flatten(null);
-    }
+    return Widget.getAllNative(view);
 
   static public inline function updateAll()
     tink.state.Observable.updateAll();
-  
+
   static public macro function hxx(e);
 
   static public macro function mount(target, markup);
 }
 
-private class H2DCursor implements Cursor<Object> {
-  
-  var pos:Int;
-  var container:Object;
+private class H2DCursor extends Cursor<Object> {
 
-  public function new(container:Object, pos:Int) {
+  var pos:Int;
+  final container:Object;
+
+  public function new(applicator, container, pos) {
+    super(applicator);
     this.container = container;
     this.pos = pos;
   }
 
-  public function insert(real:Object):Bool { 
-    var inserted = real.parent != container;
-    // if (inserted)
+  public function insert(real:Object) {
     container.addChildAt(real, pos);
-    // else if (container.childAt(pos) != real)
-    //   container.setComponentIndex(real, pos);
     pos++;
-    return inserted;
   }
 
-  public function delete():Bool
-    return 
-      if (pos <= container.numChildren) {
-        container.removeChild(current());
-        true;
-      }
-      else false;
+  public function delete(count):Void
+    if (pos == 0 && count == container.numChildren)
+      container.removeChildren();
+    else
+      for (i in 0...count)
+        container.removeChild(container.getChildAt(pos));
 
-  public function step():Bool 
+  public function step():Bool
     return
       if (pos >= container.numChildren) false;
       else ++pos == container.numChildren;
 
-  public function current():Object 
+  override public function current():Object
     return container.getChildAt(pos);
 }
 
 private class H2DBackend implements Applicator<Object> {
   public function new() {}
-  var registry:Map<Object, Rendered<Object>> = new Map();
-  
-  public function unsetLastRender(target:Object):Rendered<Object> {
-    var ret = registry[target];
-    registry.remove(target);
-    return ret;
+
+  public function siblings(target:Object):Cursor<Object>
+    return new H2DCursor(this, target.parent, target.parent.getChildIndex(target));
+
+  public function children(target:Object):Cursor<Object>
+    return new H2DCursor(this, target, 0);
+
+  public function createMarker() {
+    return new Object();
   }
 
-  public function setLastRender(target:Object, r:Rendered<Object>):Void 
-    registry[target] = r;
+  public function releaseMarker(marker) {
 
-  public function getLastRender(target:Object):Null<Rendered<Object>> 
-    return registry[target];
-
-  public function traverseSiblings(target:Object):Cursor<Object> 
-    return new H2DCursor(target.parent, target.parent.getChildIndex(target));
-
-  public function traverseChildren(target:Object):Cursor<Object> 
-    return new H2DCursor(target, 0);
-
-  public function placeholder(forTarget:Widget<Object>):VNode<Object>
-    return VNode.native(PLACEHOLDER, null, null, null, null); 
-
-  static final PLACEHOLDER = new H2DNodeType(_ -> new Object());
+  }
 }
 
-class H2DNodeType<Attr:{}, Real:Object> implements NodeType<Attr, Real> {
+class H2DNodeType<Attr:{}, Real:Object> extends Factory<Attr, Object, Real> {
 
-  var factory:Attr->Real;
+  final factory:Attr->Real;
 
-  public function new(factory) 
+  public function new(factory)
     this.factory = factory;
 
-  inline function set(target:Real, prop:String, val:Dynamic, old:Dynamic)
-    Reflect.setProperty(target, prop, val);
+  public function create(data:Attr)
+    return factory(data);
 
-  public function create(a:Attr):Real 
-    return factory(a);
+  public function update(target:Real, next:Attr, prev:Attr) {
+    var next:haxe.DynamicAccess<Any> = cast next,
+        prev:haxe.DynamicAccess<Any> = cast prev;
 
-  public function update(r:Real, old:Attr, nu:Attr):Void 
-    Differ.updateObject(r, nu, old, set);
+    for (k => v in next)
+      if (v != prev[k]) Reflect.setProperty(target, k, v);
+
+    for (k in prev.keys())
+      if (!next.exists(k)) Reflect.setProperty(target, k, null);
+  }
 }
--- src/coconut/h2d/macros/Setup.hx
+++ src/coconut/h2d/macros/Setup.hx
@@ -13,7 +13,7 @@ using haxe.io.Path;
 class Setup {
   static function all() {
 
-    var cl = Context.getType('h2d.Object').getClass(); 
+    var cl = Context.getType('h2d.Object').getClass();
     cl.meta.add(':autoBuild', [macro @:pos(cl.pos) coconut.h2d.macros.Setup.hxxAugment()], cl.pos);
   }
 
@@ -25,28 +25,28 @@ class Setup {
       case [v]:
         if (v.access == null || v.access.indexOf(APublic) == -1)
           return null;
-      default: 
+      default:
         return null;
     }
-    
+
     var self = Context.getLocalType().toComplex();//TODO: type params
-    
+
     return fields.concat((
       macro class {
-        static var COCONUT_NODE_TYPE = 
-          new coconut.h2d.Renderer.H2DNodeType<coconut.h2d.macros.Attributes<$self>, h2d.Object>(
+        static var COCONUT_NODE_TYPE =
+          new coconut.h2d.Renderer.H2DNodeType<coconut.h2d.macros.Attributes<$self>, $self>(
             attr -> coconut.h2d.macros.Instantiate.nativeView(attr)
           );
 
         static public inline function fromHxx(
-          hxxMeta:{ 
+          hxxMeta:{
             @:optional var key(default, never):coconut.diffing.Key;
             @:optional var ref(default, never):coconut.ui.Ref<$self>;
           },
-          attr:coconut.h2d.macros.Attributes<$self>, 
+          attr:coconut.h2d.macros.Attributes<$self>,
           ?children:coconut.h2d.Children):coconut.h2d.RenderResult
         {
-          return coconut.diffing.VNode.native(COCONUT_NODE_TYPE, cast hxxMeta.ref, hxxMeta.key, attr, children);
+          return COCONUT_NODE_TYPE.vnode(attr, hxxMeta.key, hxxMeta.ref, children);
         }
       }
     ).fields);
